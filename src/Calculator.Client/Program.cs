f92dbf9058546f21efa3dbd8024b4b69ee486af3
gianni <58817136+GianniGithub@users.noreply.github.com>
2026-08-15 20:04:12 +0200

    Application run now in docker and in developer enviroment.

--- src/Calculator.Client/Program.cs
+++ src/Calculator.Client/Program.cs
@@ -7,11 +7,14 @@ using Grpc.Net.Client.Web;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
 builder.Services.AddScoped(sp =>
 {
     var config = sp.GetRequiredService<IConfiguration>();
     var grpcServerUrl = config["GrpcServerUrl"]
-        ?? throw new InvalidOperationException("GrpcServerUrl ist nicht konfiguriert.");
+        ?? throw new InvalidOperationException("GrpcServerUrl is not set.");
 
     var handler = new GrpcWebHandler(
         GrpcWebMode.GrpcWeb,
@@ -23,12 +26,9 @@ builder.Services.AddScoped(sp =>
     });
 });
 
+// Typisierter gRPC-Client, der den Channel oben injiziert bekommt
 builder.Services.AddScoped(sp =>
-    new CalculatorService.CalculatorServiceClient(sp.GetRequiredService<GrpcChannel>()));
-
-builder.RootComponents.Add<App>("#app");
-builder.RootComponents.Add<HeadOutlet>("head::after");
-
-builder.Services.AddScoped(_ => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+    new CalculatorService.CalculatorServiceClient(
+        sp.GetRequiredService<GrpcChannel>()));
 
 await builder.Build().RunAsync();
